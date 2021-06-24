ed87811d23b57d088073562109dc07ecae5ab0bf
Ezequiel PR <knucquiel@gmail.com>
2021-06-24 11:25:48 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'apps-moviles_examen-1',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
